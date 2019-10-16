ee50f321e6eb9a4a84f6fb202838ce8fea5a41f1
YeLinhAung <you@example.com>
2019-10-16 10:26:28 +0630

    location change to mapview

--- app/src/main/java/com/aceplus/dms/ui/activities/customer/AddNewCustomerLocationActivity.kt
+++ app/src/main/java/com/aceplus/dms/ui/activities/customer/AddNewCustomerLocationActivity.kt
@@ -10,6 +10,7 @@ import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AlertDialog
+import android.util.Log
 import android.view.WindowManager
 import android.widget.Toast
 import com.aceplus.dms.R
@@ -22,6 +23,7 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import kotlinx.android.synthetic.main.activity_add_new_customer_location.*
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.kodein
@@ -58,6 +60,7 @@ class AddNewCustomerLocationActivity : BaseActivity(), KodeinAware {
         window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
 
         getIntentData()
+        setupUI()
         catchEvents()
     }
 
@@ -67,14 +70,51 @@ class AddNewCustomerLocationActivity : BaseActivity(), KodeinAware {
         salesmanId = intent.getStringExtra(IE_SALE_MAN_ID)
     }
 
-    private fun catchEvents(){
+    private fun setupUI(){
 
         val status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(baseContext)
         if (status != ConnectionResult.SUCCESS){
             val requestCode = 10
             GooglePlayServicesUtil.getErrorDialog(status, this, requestCode).show()
         } else{
-            val fm: SupportMapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+            mapView.getMapAsync{
+                this.map = it
+                map!!.uiSettings.isMyLocationButtonEnabled = true
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED) {
+
+                        map!!.isMyLocationEnabled = true
+                        val gpsTracker = GPSTracker(this)
+                        var lat = 0.0
+                        var lon = 0.0
+
+                        if (gpsTracker.canGetLocation()){
+                            lat = gpsTracker.getLatitude()
+                            lon = gpsTracker.getLongitude()
+                        }
+
+                        map!!.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(lat, lon), 15f))
+                        
+                    } else{
+                        checkLocationPermission() //Request Location Permission
+                    }
+                } else{
+                    map!!.isMyLocationEnabled = true
+                    val gpsTracker = GPSTracker(this)
+                    var lat = 0.0
+                    var lon = 0.0
+
+                    if (gpsTracker.canGetLocation()){
+                        lat = gpsTracker.getLatitude()
+                        lon = gpsTracker.getLongitude()
+                    }
+
+                    map!!.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(lat, lon), 15f))
+                }
+                mapView.onStart()
+            }
+            /*val fm: SupportMapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
             fm.getMapAsync {
                 this.map = it
                 map!!.uiSettings.isMyLocationButtonEnabled = true
@@ -98,12 +138,22 @@ class AddNewCustomerLocationActivity : BaseActivity(), KodeinAware {
                         checkLocationPermission() //Request Location Permission
                     }
                 }
-            }
+            }*/
         }
 
     }
 
-    val MY_PERMISSIONS_REQUEST_LOCATION = 99
+    private fun catchEvents(){
+
+        back_img.setOnClickListener { onBackPressed() }
+
+        map?.setOnMapClickListener {
+            Toast.makeText(this, it.toString(), Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    private val MY_PERMISSIONS_REQUEST_LOCATION = 99
 
     private fun checkLocationPermission(){
 
