df01900ffc64b07e3de776523b3f30a5bdb8529e
Ye Lin Aung <yelinaung2359@gmail.com>
2019-10-23 23:38:00 +0630

    adding tax and date

--- app/src/main/java/com/aceplus/dms/ui/activities/customer/sale/SaleCheckoutActivity.kt
+++ app/src/main/java/com/aceplus/dms/ui/activities/customer/sale/SaleCheckoutActivity.kt
@@ -1,5 +1,6 @@
 package com.aceplus.dms.ui.activities.customer.sale
 
+import android.app.DatePickerDialog
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
@@ -22,6 +23,9 @@ import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.kodein
 import java.text.DecimalFormat
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class SaleCheckoutActivity : BaseActivity(), KodeinAware {
     override val kodein: Kodein by kodein()
@@ -35,6 +39,7 @@ class SaleCheckoutActivity : BaseActivity(), KodeinAware {
         private const val IE_CUSTOMER_DATA = "IE_CUSTOMER_DATA"
         private const val IE_SOLD_PRODUCT_LIST = "IE_SOLD_PRODUCT_LIST"
         private const val IE_PROMOTION_LIST = "IE_PROMOTION_LIST"
+        private const val IE_SALE_EXCHANGE = "IE_SALE_EXCHANGE"
 
         fun newIntentFromSale(context: Context, customerData: Customer, soldProductList: ArrayList<SoldProductInfo>, promotionList: ArrayList<Promotion>): Intent{
             val intent = Intent(context, SaleCheckoutActivity::class.java)
@@ -55,6 +60,8 @@ class SaleCheckoutActivity : BaseActivity(), KodeinAware {
     private var totalAmount: Double = 0.0
     private var netAmount: Double = 0.0
     private var refundAmount: Double = 0.0
+    private var taxType: String = ""
+    private var taxPercent: Double = 0.0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -68,6 +75,7 @@ class SaleCheckoutActivity : BaseActivity(), KodeinAware {
 
         checkoutSoldProductListAdapter.setNewList(soldProductList)
         calculateTotalAmount()
+        setPromotionProductList()
 
     }
 
@@ -91,6 +99,7 @@ class SaleCheckoutActivity : BaseActivity(), KodeinAware {
         confirmAndPrint_img.setOnClickListener {  }
         btn_disOk.setOnClickListener { calculateDiscPercentToAmt() }
         btn_amtOk.setOnClickListener { calculateDiscAmtToPercent() }
+        edt_dueDate.setOnClickListener { chooseDueDate() }
 
         payAmount.addTextChangedListener(object :TextWatcher{
             override fun afterTextChanged(p0: Editable?) {}
@@ -112,6 +121,12 @@ class SaleCheckoutActivity : BaseActivity(), KodeinAware {
         customer = intent.getParcelableExtra(IE_CUSTOMER_DATA)
         soldProductList = intent.getParcelableArrayListExtra(IE_SOLD_PRODUCT_LIST)
         promotionList = intent.getParcelableArrayListExtra(IE_PROMOTION_LIST)
+
+    }
+
+    private fun setPromotionProductList(){
+
+        // ToDo - show promo list
 
     }
 
@@ -176,6 +191,43 @@ class SaleCheckoutActivity : BaseActivity(), KodeinAware {
 
             this.refundAmount = refundAmount
         }
+
+    }
+
+    private fun setInvoiceId(){
+
+        try {
+            val check = intent.getStringExtra(IE_SALE_EXCHANGE)
+            if (check != null && !check.equals("yes", true)){
+                // ToDo - get invoice id
+            }
+        } catch (e: NullPointerException){
+            e.printStackTrace()
+        }
+
+    }
+
+    private fun getTaxAmount(){
+        // ToDo - get tax from database
+    }
+
+    private fun calculateTax(): Double{
+        var taxAmt = 0.0
+        if (taxPercent != 0.0) taxAmt = netAmount / 21
+        return taxAmt
+    }
+
+    private fun chooseDueDate(){
+
+        val myCalendar = Calendar.getInstance()
+        val sdf = SimpleDateFormat("yyyy/MM/dd")
+
+        val dateDialog = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            myCalendar.set(year, month, dayOfMonth)
+            edt_dueDate.setText(sdf.format(myCalendar.time))
+        }, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH))
+
+        dateDialog.show()
 
     }
 
